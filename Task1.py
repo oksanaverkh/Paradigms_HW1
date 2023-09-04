9e638247963bfdcf3fa68d7968fcf4403f47d0f6
oksanaverkh <oksana-tyryatkina@yandex.ru>
2023-09-04 20:39:30 +0300

    Final commit

--- Task1.py
+++ Task1.py
@@ -1,3 +1,22 @@
 # Задача №1
 # Дан список целых чисел numbers. Необходимо написать в императивном стиле процедуру для
 # сортировки числа в списке в порядке убывания. Можно использовать любой алгоритм сортировки.
+
+import random
+
+def sort_list_imperative(numbers):
+    for i in range(len(numbers) - 1):
+        for j in range(len(numbers) - i - 1):
+            if numbers[len(numbers)-1-i] == max(numbers):
+                break
+            elif numbers[j] < numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+    return numbers
+
+
+array = []
+for i in range(10):
+    array.append(random.randint(-100, 100))
+print(array)
+
+print(sort_list_imperative(array))
--- Task2.py
+++ Task2.py
@@ -1,2 +1,15 @@
 # 💡 Задача №2
 # Написать точно такую же процедуру, но в декларативном стиле
+
+import random
+
+def sort_list_declarative(numbers):
+    return sorted(numbers, reverse=True)
+
+
+array = []
+for i in range(10):
+    array.append(random.randint(-100, 100))
+print(array)
+
+print(sort_list_declarative(array))
